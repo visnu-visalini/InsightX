85c32d2ebd91a7ecb94083464c615d0246c6f9d0
Visnu Visalini <visalinivisnu@gmail.com>
2026-01-23 15:29:28 +0530

    Day 2: Popup UI and styling

--- extensions/popup.js
+++ extensions/popup.js
@@ -1,3 +1,6 @@
-document.getElementById("testBtn").addEventListener("click", () => {
-  alert("Popup JS is working!");
+const analyzeBtn = document.getElementById("analyzeBtn");
+const output = document.getElementById("output");
+
+analyzeBtn.addEventListener("click", () => {
+  output.innerHTML = "<p>Analyzing page...</p>";
 });
